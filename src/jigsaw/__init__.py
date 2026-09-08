f57c8f3b90798a23ebb4441e150b0830a3672411
Matin Monshizadeh <matinmonshizadeh@gmail.com>
2026-09-09 00:19:52 +0200

    Declare the public API with __all__ instead of noqa re-exports

--- src/jigsaw/__init__.py
+++ src/jigsaw/__init__.py
@@ -2,8 +2,20 @@
 
 __version__ = "1.0.0"
 
-from .data import (PatchGenerator, SeededPatchGenerator, known_pixel_mask, load_stl10,  # noqa: F401
+from .data import (PatchGenerator, SeededPatchGenerator, known_pixel_mask, load_stl10,
                    set_seed, split)
-from .evaluate import evaluate, mean_patch_baseline, metrics_table, save_error_maps  # noqa: F401
-from .models import build_ordernet, build_patch_encoder, build_refinenet  # noqa: F401
-from .trainer import JigsawTrainer, TauAnneal, build_trainer, count_trainable, make_callbacks  # noqa: F401
+from .evaluate import evaluate, mean_patch_baseline, metrics_table, save_error_maps
+from .models import build_ordernet, build_patch_encoder, build_refinenet
+from .trainer import JigsawTrainer, TauAnneal, build_trainer, count_trainable, make_callbacks
+
+__all__ = [
+    "__version__",
+    # data
+    "PatchGenerator", "SeededPatchGenerator", "known_pixel_mask", "load_stl10", "set_seed", "split",
+    # models
+    "build_patch_encoder", "build_ordernet", "build_refinenet",
+    # trainer
+    "JigsawTrainer", "TauAnneal", "build_trainer", "count_trainable", "make_callbacks",
+    # evaluation
+    "evaluate", "mean_patch_baseline", "metrics_table", "save_error_maps",
+]
